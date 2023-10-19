c1271bec6d58de2fa2ca6e99049cfaf6995adb2d
Dmitry Glazkov <glazkov-98@mail.ru>
2023-10-19 17:43:05 +0600

    delete users

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -11,30 +11,6 @@ export const useUsersStore = defineStore('users', () => {
             vkId: 754937591,
             avatar: '/static/avatar1.png',
             status: 'Профи знакомств'
-        },
-        {
-            id: 2,
-            name: 'Евгений Боровицкий',
-            phone: '+7 (950) 218-04-44',
-            vkId: 364433107,
-            avatar: '/static/avatar2.png',
-            status: 'Профи знакомств'
-        },
-        {
-            id: 3,
-            name: 'Антон Петров',
-            phone: '+7 (950) 211-34-05',
-            vkId: 182526613,
-            avatar: '/static/avatar3.png',
-            status: 'Профи знакомств'
-        },
-        {
-            id: 3,
-            name: 'Павел Савин',
-            phone: '+7 (913) 662-35-74',
-            vkId: 612882358,
-            avatar: '/static/avatar4.png',
-            status: 'Профи знакомств'
         }
     ])
     function getUserForId(id: number) {
